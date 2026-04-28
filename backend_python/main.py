18f88a94c61bf7dda58a6b5ea2e3878044d0e84c
jacoboDev <jacobo.develop@gmail.com>
2026-04-28 14:21:01 +0200

    Add Exponential Backoff with CAP for 8 segs for Python Backend

--- backend_python/main.py
+++ backend_python/main.py
@@ -1,14 +1,44 @@
 import os
 import shutil
+import time
+import random
 import google.generativeai as genai
 from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
+
+
 # 1. Función para obtener la clave de forma segura
 def get_api_key():
     key = os.getenv("GEMINI_API_KEY")
     return key
+
+
+# -- Exponential Backoff con CAP en Python -- #
+async def generar_con_backoff(model, contenido, max_intentos=6):
+    espera = 2.0 # segundos
+    for i in range(max_intentos):
+        try:
+            # Intentamos generar el contenido
+            return model.generate_content(contenido)
+        except Exception as e:
+            # Si es el último intento, nos rendimos
+            if i == max_intentos -1:
+                raise e
+            
+            # Calculamos espera + jitter (azar)
+            tiempo_total = espera + random.uniform(0, 1)
+            print(f"Python: Error en Gemini. Reintento {i+1} en {int(tiempo_total)}s...")
+            
+            time.sleep(tiempo_total)
+            
+            # Lógica del CAP a 8 segundos
+            if espera < 8:
+                espera *= 2
+            continue
+    
+
 
 @app.post("/analizar")
 async def analizar_pdf(cv: UploadFile = File(...)):
@@ -30,12 +60,16 @@ async def analizar_pdf(cv: UploadFile = File(...)):
         file_uploaded = genai.upload_file(path=temp_path, display_name="CV_Python")
         model = genai.GenerativeModel("gemini-2.5-flash")
         
-        response = model.generate_content([
+        prompt = [
             file_uploaded,
             "Analiza este CV y devuelve un JSON con: nombre_completo, email, habilidades_principales y resumen_perfil."
-        ])
+        ]
+
+        response = await generar_con_backoff(model, prompt)
         
         return {"data": response.text}
+    except Exception as e:
+        return {"error": str(e)}       
     finally:
         if os.path.exists(temp_path):
             os.remove(temp_path)
